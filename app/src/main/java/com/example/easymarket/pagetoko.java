a94c6f6f865177158efceff85894cef98fe8d599
Agung <agungsigungan@gmail.com>
2019-09-20 07:17:03 +0700

    layout toko

---
+++ app/src/main/java/com/example/easymarket/pagetoko.java
@@ -0,0 +1,14 @@
+package com.example.easymarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pagetoko extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagetoko);
+    }
+}
