e7bc351a48e225bb2f65235d89bcc8c7fc418e4c
alvinsucita <alvinsucita@gmail.com>
2019-09-20 07:50:08 +0700

    support page

---
+++ app/src/main/java/com/example/easymarket/support_page.java
@@ -0,0 +1,14 @@
+package com.example.easymarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class support_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support_page);
+    }
+}
