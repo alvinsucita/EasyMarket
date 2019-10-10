be09fe53cb2322bfb2d5cca9310bc12f524b0209
Agung <agungsigungan@gmail.com>
2019-10-10 14:07:40 +0700

    update card view product, review, info

---
+++ app/src/main/java/com/example/easymarket/info_toko.java
@@ -0,0 +1,14 @@
+package com.example.easymarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class info_toko extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_toko);
+    }
+}
