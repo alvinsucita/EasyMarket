c328c8f323c393e1f743ff7a85418f314e5b52fd
Adam Kahardja <kahardjaa@gmail.com>
2019-10-25 00:54:36 +0700

    adding preview produk activity, pemesanan activity,milih barang activity

--- app/src/main/java/com/example/easymarket/CekPerjalananActivity.java
+++ app/src/main/java/com/example/easymarket/CekPerjalananActivity.java
@@ -7,17 +7,31 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CekPerjalananActivity extends AppCompatActivity {
 
     Button btnCekPerjalan;
+    TextView tvID,tvAlamat,tvKeterangan;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cek_perjalanan);
 
+        tvID = findViewById(R.id.tvIdBarangCekPerjalanan);
+        tvAlamat = findViewById(R.id.tvAlamatCekPerjalanan);
+        //tvJasaPengirim = findViewById(R.id.spinnerspinnerJasaPengirimCekPerjalan);
         btnCekPerjalan = findViewById(R.id.btnCekPerjalanan);
+        tvKeterangan = findViewById(R.id.tvKeteranganCekPerjalanan);
 
         GradientDrawable drawable = new GradientDrawable();
         drawable.setShape(GradientDrawable.RECTANGLE);
@@ -26,6 +40,47 @@ public class CekPerjalananActivity extends AppCompatActivity {
         btnCekPerjalan.setBackground(drawable);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Spinner element
+        spinner = findViewById(R.id.spinnerJasaPengirimCekPerjalan);
+        // Spinner click listener
+        //spinner.setOnItemSelectedListener();
+        List<String> categories = new ArrayList<String>();
+        categories.add("JNE");
+        categories.add("TIKI");
+        categories.add("POS INDONESIA");
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+//        @Override
+//        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//            // On selecting a spinner item
+//            String item = parent.getItemAtPosition(position).toString();
+//
+//            // Showing selected spinner item
+//            Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+//        }
+//        public void onNothingSelected(AdapterView<?> arg0) {
+//            // TODO Auto-generated method stub
+//        }
+
+        final Boolean cek = true;
+        btnCekPerjalan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cek){
+                    tvKeterangan.setText("Terkirim");
+                }
+                else{
+                    tvKeterangan.setText("BelumTerkirim");
+                }
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/easymarket/RefundActivity.java
+++ app/src/main/java/com/example/easymarket/RefundActivity.java
@@ -6,17 +6,58 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RefundActivity extends AppCompatActivity {
 
     Button btnSubmit;
+    TextView tvID,tvNama,tvEmail,tvRekening;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refund);
 
         btnSubmit = findViewById(R.id.btnSubmit);
+        tvNama = findViewById(R.id.tvNamaPemesan);
+        tvID = findViewById(R.id.tvIdBarang);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvRekening = findViewById(R.id.tvRekening);
+
+        // Spinner element
+        spinner = findViewById(R.id.spinnerPilihBank);
+        // Spinner click listener
+        //spinner.setOnItemSelectedListener();
+        List<String> categories = new ArrayList<String>();
+        categories.add("BCA");
+        categories.add("MANDIRI");
+        categories.add("DANAMON");
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+//        @Override
+//        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//            // On selecting a spinner item
+//            String item = parent.getItemAtPosition(position).toString();
+//
+//            // Showing selected spinner item
+//            Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+//        }
+//        public void onNothingSelected(AdapterView<?> arg0) {
+//            // TODO Auto-generated method stub
+//        }
 
         GradientDrawable drawable = new GradientDrawable();
         drawable.setShape(GradientDrawable.RECTANGLE);
@@ -25,6 +66,15 @@ public class RefundActivity extends AppCompatActivity {
         btnSubmit.setBackground(drawable);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tvNama.getText().toString() == "" && tvID.getText().toString() == "" && tvEmail.getText().toString() == ""){
+                    Toast.makeText(getApplicationContext(),"DONE",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
     @Override
