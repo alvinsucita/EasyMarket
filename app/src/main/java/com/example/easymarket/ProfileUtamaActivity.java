9fa5e022b09ffb57ac5ec213b3c834a0ea4754ae
felix <felix_lonald@hotmail.com>
2019-12-04 08:51:50 +0700

    ok

--- app/src/main/java/com/example/easymarket/ProfileUtamaActivity.java
+++ app/src/main/java/com/example/easymarket/ProfileUtamaActivity.java
@@ -106,14 +106,12 @@ public class ProfileUtamaActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
         if(requestCode==1 && data!=null){
             Uri selected = data.getData();
             profile.setImageURI(selected);
-            Toast.makeText(this, selected.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 
