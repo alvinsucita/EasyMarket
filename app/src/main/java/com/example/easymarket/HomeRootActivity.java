54290dbf00174d13112dd2aecc44f98c28966249
Adam Kahardja <kahardjaa@gmail.com>
2019-12-04 15:36:43 +0700

    HomeRootActivityUpdate

--- app/src/main/java/com/example/easymarket/HomeRootActivity.java
+++ app/src/main/java/com/example/easymarket/HomeRootActivity.java
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,7 +22,7 @@ public class HomeRootActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_root);
 
-        
+
         changeFragment(new FragmentHome());
         bottomNavigationView=findViewById(R.id.btmNavRootHome);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,8 +49,88 @@ public class HomeRootActivity extends AppCompatActivity {
     public void changeFragment(Fragment f){
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.fragmentContainer, f);
+        ft.replace(R.id.fragmentContainerRootHome, f);
         ft.commit();
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.option_menu,menu);
+//        if(aktif.equals("0")){
+//            menu.getItem(0).setVisible(true);
+//            menu.getItem(1).setVisible(false);
+//            menu.getItem(2).setVisible(false);
+//            menu.getItem(3).setVisible(false);
+//            menu.getItem(4).setVisible(false);
+//            menu.getItem(5).setVisible(false);
+//            menu.getItem(6).setVisible(false);
+//            menu.getItem(7).setVisible(false);
+//            menu.getItem(8).setVisible(false);
+//        }
+//        else{
+//            menu.getItem(3).setTitle("Hai, "+userlogin+" !");
+//            menu.getItem(3).setVisible(true);
+//            menu.getItem(0).setVisible(false);
+//            menu.getItem(1).setVisible(true);
+//            menu.getItem(2).setVisible(true);
+//            menu.getItem(4).setVisible(true);
+//            menu.getItem(5).setVisible(true);
+//            menu.getItem(6).setVisible(true);
+//            menu.getItem(7).setVisible(false);
+//            menu.getItem(8).setVisible(true);
+//        }
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId()==R.id.itemLogin){
+//            Intent i = new Intent(Home.this,Login.class);
+//            i = putextra(i);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId()==R.id.itemLogout){
+//            Intent i = new Intent(Home.this,Home.class);
+//            for (int j = 0; j < listUser.size(); j++) {
+//                if(listUser.get(j).aktif.equals("1")){
+//                    listUser.get(j).aktif="0";
+//                    i = putextra(i);
+//                    startActivity(i);
+//                }
+//            }
+//        }
+//        else if(item.getItemId()==R.id.itemEvent){
+//            Intent i = new Intent(HomeRootActivity.this,EventPage.class);
+//            i = putextra(i);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId() == R.id.itemNama){
+//            Intent i = new Intent(HomeRootActivity.this,ProfileUtamaActivity.class);
+//            i = putextra(i);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId()==R.id.itemWishlist){
+//            Intent i = new Intent(HomeRootActivity.this,WishList.class);
+//            i = putextra(i);
+//            i.putExtra("adayanglogin",aktif);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId()==R.id.itemLelang){
+//            Intent i = new Intent(HomeRootActivity.this,LelangActivity.class);
+//            i = putextra(i);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId()==R.id.itemCekPengiriman){
+//            Intent i = new Intent(HomeRootActivity.this,CekPerjalananActivity.class);
+//            startActivity(i);
+//        }
+//        else if(item.getItemId()==R.id.itemRefund){
+//            Intent i = new Intent(HomeRootActivity.this,RefundActivity.class);
+//            startActivity(i);
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
 
 }
