87b92093b88dfe7564b0e9952f2f179a3605cbcb
Agung <agungsigungan@gmail.com>
2019-12-13 09:12:12 +0700

    tampilan event, barang lelang, icon

--- app/src/main/java/com/example/easymarket/FragmentEvent.java
+++ app/src/main/java/com/example/easymarket/FragmentEvent.java
@@ -1,9 +1,15 @@
 package com.example.easymarket;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,4 +34,28 @@ public class FragmentEvent extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_event, container, false);
     }
 
+    RecyclerView rv;
+    AdapterEvent adapter;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final Home home = (Home)getActivity();
+
+        rv = view.findViewById(R.id.rvevent);
+
+        GradientDrawable drawable3 = new GradientDrawable();
+        drawable3.setShape(GradientDrawable.RECTANGLE);
+        drawable3.setCornerRadius(25);
+        drawable3.setStroke(8, Color.LTGRAY);
+        drawable3.setColor(Color.WHITE);
+        rv.setBackground(drawable3);
+
+        final AdapterEvent adapterEvent = new AdapterEvent();
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(adapterEvent);
+
+    }
 }
