17257c5020f19d44492fa07f61d04dc1744a1482
alvinsucita <alvinsucita@gmail.com>
2019-12-13 10:19:26 +0700

    admin bisa ngeban

--- app/src/main/java/com/example/easymarket/AdapterMenuAdmin.java
+++ app/src/main/java/com/example/easymarket/AdapterMenuAdmin.java
@@ -5,19 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AdapterMenuAdmin extends ArrayAdapter {
     private final Activity context;
     private final String[] judul;
-    private final String[] desc;
-    public AdapterMenuAdmin(Activity context, String[] judul, String[] desc){
-
+    public AdapterMenuAdmin(Activity context, String[] judul){
         super(context,R.layout.item_textonlyadmin, judul);
         this.context = context;
         this.judul = judul;
-        this.desc = desc;
     }
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
@@ -25,7 +21,7 @@ public class AdapterMenuAdmin extends ArrayAdapter {
         TextView nameTextField = (TextView) rowView.findViewById(R.id.layoutjudul);
         TextView infoTextField = (TextView) rowView.findViewById(R.id.layoutdesc);
         nameTextField.setText(judul[position]);
-        infoTextField.setText(desc[position]);
+        infoTextField.setText("");
         return rowView;
     };
 }
