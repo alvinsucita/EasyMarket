b7e0cf74aff73f543c20b5ff9f40e4c3e2385bac
Agung <agungsigungan@gmail.com>
2019-12-16 15:34:07 +0700

    update fragment ditunda

--- app/src/main/java/com/example/easymarket/FragmentDitunda.java
+++ app/src/main/java/com/example/easymarket/FragmentDitunda.java
@@ -2,6 +2,8 @@ package com.example.easymarket;
 
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,9 +36,26 @@ public class FragmentDitunda extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_ditunda, container, false);
     }
 
+    RecyclerView rvditunda;
+    AdapterBatal adapterditunda;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        final HistoryActivity historyActivity = (HistoryActivity)getActivity();
+
+        rvditunda = view.findViewById(R.id.rvDitunda);
+
+        GradientDrawable drawable3 = new GradientDrawable();
+        drawable3.setShape(GradientDrawable.RECTANGLE);
+        drawable3.setCornerRadius(25);
+        drawable3.setStroke(8, Color.LTGRAY);
+        drawable3.setColor(Color.WHITE);
+        rvditunda.setBackground(drawable3);
+
+        rvditunda.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvditunda.setAdapter(adapterditunda);
     }
 
 
