0d3c1046b3e1380019f5f3294301239c2972c59f
alvinsucita <alvinsucita@gmail.com>
2019-12-16 13:59:43 +0700

    Sandbox PayPal (di fragment ditunda)

---
+++ app/src/main/java/com/example/easymarket/Config/Config.java
@@ -0,0 +1,6 @@
+package com.example.easymarket.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "ATRWlMz_xmEwT9okPSGAp7f-txnZPiqcbhnopk4yhjDDSZrZ7KzZPJARcb9XGhx9tQ4Ymj5dK7gkKwJM";
+
+}
