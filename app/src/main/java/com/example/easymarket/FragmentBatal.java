6a5a20e4bdf088dd7f23746d864de312ff104a37
Agung <agungsigungan@gmail.com>
2019-12-16 12:57:42 +0700

    tambah recycler n adapter history, update fragment history

--- app/src/main/java/com/example/easymarket/FragmentBatal.java
+++ app/src/main/java/com/example/easymarket/FragmentBatal.java
@@ -1,9 +1,15 @@
 package com.example.easymarket;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +26,6 @@ public class FragmentBatal extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,4 +33,25 @@ public class FragmentBatal extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_batal, container, false);
     }
 
+    RecyclerView rvbatal;
+    AdapterBatal adapterbatal;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final HistoryActivity historyActivity = (HistoryActivity)getActivity();
+
+        rvbatal = view.findViewById(R.id.rvBatal);
+
+        GradientDrawable drawable3 = new GradientDrawable();
+        drawable3.setShape(GradientDrawable.RECTANGLE);
+        drawable3.setCornerRadius(25);
+        drawable3.setStroke(8, Color.LTGRAY);
+        drawable3.setColor(Color.WHITE);
+        rvbatal.setBackground(drawable3);
+
+        rvbatal.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvbatal.setAdapter(adapterbatal);
+    }
 }
--- app/src/main/java/com/example/easymarket/FragmentMenunggu.java
+++ app/src/main/java/com/example/easymarket/FragmentMenunggu.java
@@ -1,9 +1,15 @@
 package com.example.easymarket;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +32,28 @@ public class FragmentMenunggu extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_menunggu, container, false);
+    }
+
+    RecyclerView rvmenunggu;
+    AdapterBatal adaptermenunggu;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final HistoryActivity historyActivity = (HistoryActivity)getActivity();
+
+        rvmenunggu = view.findViewById(R.id.rvMenunggu);
+
+        GradientDrawable drawable3 = new GradientDrawable();
+        drawable3.setShape(GradientDrawable.RECTANGLE);
+        drawable3.setCornerRadius(25);
+        drawable3.setStroke(8, Color.LTGRAY);
+        drawable3.setColor(Color.WHITE);
+        rvmenunggu.setBackground(drawable3);
+
+        rvmenunggu.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvmenunggu.setAdapter(adaptermenunggu);
     }
 
 }
