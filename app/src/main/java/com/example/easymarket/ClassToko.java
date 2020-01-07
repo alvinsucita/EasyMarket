96571820dba4a45afd8e3b823b058f3d52151e43
alvinsucita <alvinsucita@gmail.com>
2020-01-07 13:34:39 +0700

    verifikasi toko?

--- app/src/main/java/com/example/easymarket/ClassToko.java
+++ app/src/main/java/com/example/easymarket/ClassToko.java
@@ -8,14 +8,19 @@ public class ClassToko implements Serializable {
     String password;
     String aktif;
     String FirebaseUID;
+    String daerahasal;
+    int rating;
 
     public ClassToko(){}
-    public ClassToko(String nama, String email, String password, String aktif, String firebaseUID) {
+
+    public ClassToko(String nama, String email, String password, String aktif, String firebaseUID, String daerahasal, int rating) {
         this.nama = nama;
         this.email = email;
         this.password = password;
         this.aktif = aktif;
         FirebaseUID = firebaseUID;
+        this.daerahasal = daerahasal;
+        this.rating = rating;
     }
 
     public String getNama() {
@@ -57,4 +62,19 @@ public class ClassToko implements Serializable {
     public void setFirebaseUID(String firebaseUID) {
         FirebaseUID = firebaseUID;
     }
-}
+
+    public String getDaerahasal() {
+        return daerahasal;
+    }
+
+    public void setDaerahasal(String daerahasal) {
+        this.daerahasal = daerahasal;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }}
